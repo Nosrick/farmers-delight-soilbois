2867a01c0ab1e381cbc78f35a988a29990f918e1
nosrick <nosrick@gmail.com>
2022-03-04 19:23:21 +0000

    Tidying up

--- src/main/java/com/gitlab/nosrick/soilbois/mixin/VillagerAccess.java
+++ src/main/java/com/gitlab/nosrick/soilbois/mixin/VillagerAccess.java
@@ -6,7 +6,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import java.util.Map;
-import java.util.Set;
 
 @Mixin(VillagerEntity.class)
 public interface VillagerAccess {
